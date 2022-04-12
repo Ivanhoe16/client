02b066142c72a65fd3e9b008e933520db8ee058c
Ivanhoe16 <50368394+Ivanhoe16@users.noreply.github.com>
2022-04-13 02:36:02 +0300

    Initial commit

---
+++ lab_4 sem/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace lab_4_sem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.error = new System.Windows.Forms.Label();
+            this.excelButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.phoneTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DBTable = new System.Windows.Forms.DataGridView();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.phone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.readedTextBox = new System.Windows.Forms.TextBox();
+            this.loadPictureButton = new System.Windows.Forms.Button();
+            this.readButton = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DBTable)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(620, 353);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.error);
+            this.tabPage1.Controls.Add(this.excelButton);
+            this.tabPage1.Controls.Add(this.addButton);
+            this.tabPage1.Controls.Add(this.phoneTextBox);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.nameTextBox);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.DBTable);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(612, 327);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Работа с БД";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // error
+            // 
+            this.error.AutoSize = true;
+            this.error.Location = new System.Drawing.Point(138, 214);
+            this.error.Name = "error";
+            this.error.Size = new System.Drawing.Size(0, 13);
+            this.error.TabIndex = 8;
+            // 
+            // excelButton
+            // 
+            this.excelButton.Location = new System.Drawing.Point(489, 243);
+            this.excelButton.Name = "excelButton";
+            this.excelButton.Size = new System.Drawing.Size(113, 23);
+            this.excelButton.TabIndex = 7;
+            this.excelButton.Text = "Выгрузить Excel";
+            this.excelButton.UseVisualStyleBackColor = true;
+            this.excelButton.Click += new System.EventHandler(this.excelButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(489, 214);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(113, 23);
+            this.addButton.TabIndex = 6;
+            this.addButton.Text = "Добавить";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // phoneTextBox
+            // 
+            this.phoneTextBox.Location = new System.Drawing.Point(141, 188);
+            this.phoneTextBox.Name = "phoneTextBox";
+            this.phoneTextBox.Size = new System.Drawing.Size(461, 20);
+            this.phoneTextBox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Номер телефона";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(141, 162);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(461, 20);
+            this.nameTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Фамилия имя отчество";
+            // 
+            // DBTable
+            // 
+            this.DBTable.AllowUserToAddRows = false;
+            this.DBTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DBTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.name,
+            this.phone});
+            this.DBTable.Location = new System.Drawing.Point(6, 6);
+            this.DBTable.Name = "DBTable";
+            this.DBTable.RowHeadersVisible = false;
+            this.DBTable.Size = new System.Drawing.Size(600, 150);
+            this.DBTable.TabIndex = 0;
+            // 
+            // name
+            // 
+            this.name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.name.HeaderText = "ФИО";
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            // 
+            // phone
+            // 
+            this.phone.HeaderText = "Номер телефона";
+            this.phone.Name = "phone";
+            this.phone.ReadOnly = true;
+            this.phone.Width = 200;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.readButton);
+            this.tabPage2.Controls.Add(this.loadPictureButton);
+            this.tabPage2.Controls.Add(this.readedTextBox);
+            this.tabPage2.Controls.Add(this.pictureBox);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(612, 327);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Распознавание текста";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(6, 42);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(289, 282);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // readedTextBox
+            // 
+            this.readedTextBox.Location = new System.Drawing.Point(301, 42);
+            this.readedTextBox.Multiline = true;
+            this.readedTextBox.Name = "readedTextBox";
+            this.readedTextBox.Size = new System.Drawing.Size(301, 282);
+            this.readedTextBox.TabIndex = 1;
+            // 
+            // loadPictureButton
+            // 
+            this.loadPictureButton.Location = new System.Drawing.Point(8, 13);
+            this.loadPictureButton.Name = "loadPictureButton";
+            this.loadPictureButton.Size = new System.Drawing.Size(152, 23);
+            this.loadPictureButton.TabIndex = 2;
+            this.loadPictureButton.Text = "Загрузить изображение";
+            this.loadPictureButton.UseVisualStyleBackColor = true;
+            this.loadPictureButton.Click += new System.EventHandler(this.loadPictureButton_Click);
+            // 
+            // readButton
+            // 
+            this.readButton.Location = new System.Drawing.Point(166, 13);
+            this.readButton.Name = "readButton";
+            this.readButton.Size = new System.Drawing.Size(110, 23);
+            this.readButton.TabIndex = 3;
+            this.readButton.Text = "Конвертировать";
+            this.readButton.UseVisualStyleBackColor = true;
+            this.readButton.Click += new System.EventHandler(this.readButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 351);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DBTable)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button excelButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox phoneTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView DBTable;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn phone;
+        private System.Windows.Forms.Label error;
+        private System.Windows.Forms.Button readButton;
+        private System.Windows.Forms.Button loadPictureButton;
+        private System.Windows.Forms.TextBox readedTextBox;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ lab_4 sem/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+using Tesseract;
+using System.Net.Sockets;
+
+namespace lab_4_sem
+{
+    public partial class Form1 : Form
+    {
+        private string picturePath = "";
+        private string host = "127.0.0.1";
+        private int port = 8085;
+
+        public Form1()
+        {
+            InitializeComponent();
+            getTable();
+        }
+
+        private void loadTable()
+        {
+            while (DBTable.RowCount > 0)
+                DBTable.Rows.RemoveAt(0);
+            getTable();
+        }
+        //post sampler
+        private void sendData(string name, string phone)
+        {
+            error.Text = "";
+            try
+            {
+                IPEndPoint point = new IPEndPoint(IPAddress.Parse(host), port);
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(point);
+                string message = name + ";" + phone + ";";
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                socket.Send(data);
+                socket.Close();
+            }catch (Exception e)
+            {
+                error.Text = "Ошибка подключения";
+            }
+        }
+        private void getTable()
+        {
+            error.Text = "";
+            try
+            {
+                IPEndPoint point = new IPEndPoint(IPAddress.Parse(host), port);
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(point);
+                byte[] data = Encoding.Unicode.GetBytes("get");
+                socket.Send(data);
+                data = new byte[256];
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0;
+                do
+                {
+                    bytes = socket.Receive(data, data.Length, 0);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                } while (socket.Available > 0);
+                string a = builder.ToString();
+                string[] phones = builder.ToString().Split(';');
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+                for (int i = 0; i < phones.Length - 1; i += 2)
+                    DBTable.Rows.Add(phones[i], phones[i + 1]);
+            }catch (Exception e)
+            {
+                error.Text = "Ошибка подключения";
+            }
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedIndex == 0)
+                loadTable();
+        }
+        //добавить в БД
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            sendData(nameTextBox.Text, phoneTextBox.Text);
+            nameTextBox.Text = phoneTextBox.Text = "";
+            loadTable();
+        }
+        //загрузить в эксель
+        private void excelButton_Click(object sender, EventArgs e)
+        {
+            Excel.Application application = new Excel.Application();
+            Excel.Workbook workbook;
+            Excel.Worksheet worksheet;
+            object misValue = System.Reflection.Missing.Value;
+            workbook = application.Workbooks.Add(misValue);
+            worksheet = workbook.ActiveSheet;
+            worksheet.Name = "2022";
+            worksheet.Cells[1, 1] = "Фамилия Имя Отчество";
+            worksheet.Cells[1, 2] = "Номер телефона";
+            for (int i = 0; i < DBTable.RowCount; ++i)
+            {
+                worksheet.Cells[i + 2, 1] = DBTable.Rows[i].Cells[0].Value;
+                worksheet.Cells[i + 2, 2] = DBTable.Rows[i].Cells[1].Value;
+            }
+            workbook.SaveAs("адресная книга.xls", Excel.XlFileFormat.xlWorkbookNormal,misValue,misValue,misValue,misValue,Excel.XlSaveAsAccessMode.xlExclusive,misValue,misValue,misValue,misValue,misValue);
+            workbook.Close(true, misValue, misValue);
+            application.Quit();
+        }
+        //загрузка изображения
+        private void loadPictureButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp; *.png)|*.jpg; *.jpeg; *.gif; *.bmp; *.png";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox.Image = new Bitmap(open.FileName);
+                picturePath = open.FileName;
+            }
+        }
+        //преобразование изображения в текст
+        private void readButton_Click(object sender, EventArgs e)
+        {
+            var engine = new TesseractEngine("C:\\Users\\user\\Desktop\\универ\\мага\\lab_4 sem\\lab_4 sem\\bin\\Debug\\tessdata", "rus+eng", EngineMode.Default);
+            var picture = Pix.LoadFromFile(picturePath);
+            var result = engine.Process(picture);
+            readedTextBox.Text = result.GetText();
+        }
+    }
+}
